6a370b6f1855c0668b9bf50591e317bd67a03b8d
sato gaito <gaito4570@gmail.com>
2020-01-10 22:00:32 +0900

    [Finish] 編集機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -15,8 +15,19 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to books_path
+    redirect_to book_path(book)
   end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book)
+  end
+
 
 
 
